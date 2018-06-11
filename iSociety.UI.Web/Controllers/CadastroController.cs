02436f07d1475664fd7de8c18f84a3b3ef9a8ee8
erick <erick@DESKTOP-TCMH85K>
2018-06-11 17:35:19 -0300

    Adicionada Criptografia Hash

--- iSociety.UI.Web/Controllers/CadastroController.cs
+++ iSociety.UI.Web/Controllers/CadastroController.cs
@@ -1,5 +1,6 @@
 ﻿using iSociety.Models;
 using System.Web.Mvc;
+using iSociety.Repositorio;
 
 namespace iSociety.UI.Web.Controllers
 {
@@ -13,12 +14,14 @@ namespace iSociety.UI.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize]
         public ActionResult Index(UsuarioConsumidor user)
         {
             if (ModelState.IsValid)
             {
+                var cripto = new Cripto();
                 var usuario = new QueryUsuarioConsumidor();
+                var senhaHash = cripto.Hash(user.Senha);
+                user.Senha = senhaHash;
                 usuario.Inserir(user);
                 return RedirectToAction("Index");
                 //return RedirectToRoute(new{ controller = "Home", action = "About", id = UrlParameter.Optional });
